bb286009f59b05fe99d678f02471158caac5256c
Dante.L <dantel@D.L>
2020-08-02 22:48:55 +0300

    incorperated the use of if else functions as well as inlining an array in my function

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,34 @@
 var pizzasizeprice, crustprice, total;
+$(document).ready(function(){
+    $("#myprice").click(function(event){
+        $(".show-total").show();
+        var size = $("#flavor option:selected").val();
+        var crust = $("#crust option:selected").val();
+        var quantity = parseInt($("#quantity").val());
+        var topping =[];
+        $.each($('input[name="topping"]:checked'),function(){
+            topping.push($(this).val());
+        });
+    if (crust == "none"){
+        crustprice=0;
+      }
+ else if (crust == "Pan-crust"){
+ crustprice = 1 ;
+ }else if (crust == "Original Thin Crust"){
+     crustprice =1.50;
+ }else if (crust == "Gluten Free"){
+     crustprice =2.00;
+ }else {
+     crustprice=2.00;
+ }
+
+
+ if(size =="regular"){
+     pizzasizeprice = 5;
+ } else if (size == "medium") {
+     pizzasizeprice=7;
+ }  else if(size == "mega"){
+     pizzasizeprice = 10;
+ }else{
+     pizzasizeprice=0;
+ }
